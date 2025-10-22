1bc3ab662934942de9c9cd76bf71dc17a7b2140a
SheFing <shevchukm2002@gmail.com>
2025-10-22 15:22:00 +0300

    att1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum BtnType {
+  Nothing = '',
+  All = 'All',
+  Five = 'Five',
+  Red = 'Red',
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [changeListOfGoods, setChangeListOfGoods] = useState<BtnType>();
+  const [listGoods, setListGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    let items: Promise<Good[]> | undefined;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    switch (changeListOfGoods) {
+      case BtnType.All:
+        items = getAll();
+        break;
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      case BtnType.Five:
+        items = get5First();
+        break;
+
+      case BtnType.Red:
+        items = getRedGoods();
+        break;
+
+      default:
+        items = undefined;
+        break;
+    }
+
+    items?.then(item => setListGoods(item)).catch(() => 1 + 1);
+  }, [changeListOfGoods]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setChangeListOfGoods(BtnType.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setChangeListOfGoods(BtnType.Five)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setChangeListOfGoods(BtnType.Red)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={listGoods} />
+    </div>
+  );
+};
